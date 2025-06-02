8d5b7a2375b3bedf7d128d2808b1fc31f0483360
mithuntc <mithuntc783@gmail.com>
2025-06-02 10:48:03 +0530

    mochaawsome report plugin added

--- cypress.config.js
+++ cypress.config.js
@@ -4,9 +4,20 @@ module.exports = defineConfig({
   env: {
     baseUrl: 'https://rahulshettyacademy.com'
   },
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+                reportDir: 'cypress/reports/mochareports', // Adjust as needed
+                reportFile: 'report',  // Adjust as needed
+                reportPageTitle: 'Cypress Test Report', // Adjust as needed
+                charts: true,
+                embeddedScreenshots: true,
+                inlineAssets: true,
+                video: true
+            },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on,config);
     },
     specPattern: 'cypress/integration/e_commerce_Framework/*.js'
   },
