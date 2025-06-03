aa67fb1201ff0a7e79a94598c5219290dd8b437c
mithuntc <mithuntc783@gmail.com>
2025-06-03 15:09:23 +0530

    mocking responses from cypress is done

---
+++ cypress/integration/mocking_HTTP_Intercept/moke_HTTP.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+describe('Mocking HTTP Test Suite', function () {
+    it('Mocking HTTP and testing', () => {
+        cy.visit('https://rahulshettyacademy.com/angularAppdemo/');
+       
+        cy.intercept({
+            method: 'GET',
+            url: 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty'
+        }, {
+            // Mocking the response
+            statusCode: 200,
+            body: [{
+                "book_name": "RestAssured with Java",
+                "isbn": "LSA",
+                "aisle": "2303"
+            }]
+        }).as('bookRetrievalData');
+        // Click on the 'Library' button
+        cy.get('button.btn.btn-primary').click();
+        // Wait for the mocked HTTP request to complete
+        cy.wait('@bookRetrievalData'); // Wait for 2 seconds to ensure the request is processed
+        cy.get('p').should('have.text', 'Oops only 1 Book available'); // Verify the text on the page
+    })
+})
